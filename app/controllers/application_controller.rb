34f426e2a0c4714375463caa149880705b29604c
Matthew Chigira <43639290+matthewchigira@users.noreply.github.com>
2025-09-18 10:34:47 +0900

    AI: Update Front Page Text
    Generated by WorkItem #ad0ade10-b365-47c1-b831-9f57b62e5650

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def index
-    render json: { message: 'Hello, world!, v2' }
+    render json: { message: 'Looks like this worked.' }
   end
 end
